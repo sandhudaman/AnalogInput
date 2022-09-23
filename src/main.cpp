7fb107e855f43f3f2c3152956abd5765367ddac3
sandhudaman <sandhudaman0024@outlook.com>
2022-09-23 09:39:39 -0400

    after init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+#include <Arduino.h> 
+ 
+void setup() { 
+  // configure the USB serial monitor 
+  Serial.begin(115200); 
+} 
+ 
+void loop() { 
+  int iVal; 
+ 
+  // read digitized value from the D1 Mini's A/D convertor 
+  iVal = analogRead(A0); 
+ 
+  // print value to the USB port 
+  Serial.println("Digitized Value = " + String(iVal)); 
+ 
+  // wait 0.5 seconds (500 ms) 
+  delay(500); 
+} 
